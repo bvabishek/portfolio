b0f5d179d3085c54c18325a0c1e553742f4cd414
Abishek <88036229+bvabishek@users.noreply.github.com>
2024-06-16 18:17:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-dark' style={{height: '100%'}}>
+    <div className='bg-dark' style={{height: '100%',width: '100%'}}>
       <Header />
       <Home />
       <Projects />
